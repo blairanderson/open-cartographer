5c76ab872f582ec52c5d3ee1c859a3074cae0b60
Sam Breed <sam@quickleft.com>
2013-03-19 23:12:21 -0600

    some refactoring to cut down on Redis calls

--- routes/index.js
+++ routes/index.js
@@ -10,62 +10,62 @@ var r = config.redis;
 var client = redis.createClient(r.port, r.host, r);
 if( r.pass ) client.auth(r.pass);
 
-
 /*
- * GET home page.
+ * Retrieves open graph meta tags from a url
  */
-
-
-function processUrl(req, res){
-  var url = req.query.url;
-
+function processUrl(url, req, res){
   request(url, function(err, resp, body){
     var $ = cheerio.load(body);
-    var metatags = $('meta[property]');
+    var key = 'url:'+ url;
+    var tagList = $('meta[property]');
     var graphProperties = {};
 
-    _.each(metatags, function(tag){
+    _.each(tagList, function(tag){
       var $tag = $(tag);
       var prop = $tag.attr('property');
       var content = $tag.attr('content');
+
+      if( /^\d+$/.test(content) ){
+        content = parseInt(content, 10);
+      }
 
       if( prop && content ){
         graphProperties[ prop ] = content;
       }
     });
 
-    client.hmset('url:'+ url, graphProperties, function(err){ });
-    client.expire('url:'+ url, 86400);
+    client.multi()
+      .hmset(key, graphProperties)
+      .expire(key, 86400)
+      .exec(function(err){
+        console.log(err);
+      });
 
     res.json(graphProperties);
   });
 }
 
+/*
+ * GET home page.
+ */
+
 exports.index = function(req, res){
-  if( req.query && req.query.url ){
-
-    var url = req.query.url;
-    client.exists('url:'+ url, function(err, exists){
-      if( err ){
-        res.send(500);
-        return;
-      }
-
-      if( exists ){
-        client.hgetall('url:'+ url, function(err, result){
-          if( err ){
-            res.send(500);
-            return;
-          }
-          res.json(result);
-        });
-      } else {
-        processUrl(req, res);
-      }
-
-    });
-
-  } else {
+  var url = req.query.url;
+  if( !url ){
     res.send(412);
+    return;
   }
+
+  client.hgetall('url:'+ url, function(err, result){
+    if( err ){
+      res.send(500);
+      return;
+    }
+
+    if( result === null ){
+      processUrl(url, req, res);
+    } else {
+      res.json(result);
+    }
+  });
 };
