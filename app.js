3627575698fd570d5e3b0f8e9cd8ed295ab4ab4b
Sam Breed <sam@quickleft.com>
2013-09-28 20:11:31 -0600

    adding routes for embeds

--- app.js
+++ app.js
@@ -29,6 +29,24 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+function handleJSON(err, graphProperties){
+  if( err ){
+    console.error('Error: '+ err +'|url: '+ this.url);
+    return this.res.json(500, { error: 'An error occurred processing: ' + this.url });
+  }
+
+  if( this.req.query && this.req.query.callback ){
+    this.res.send(this.res.query.callback +'('+ JSON.stringify(graphProperties) +')');
+  } else {
+    if( graphProperties.embed ){
+      try {
+        graphProperties.embed = JSON.parse(graphProperties.embed);
+      } catch(e){ }
+    }
+    this.res.json(graphProperties);
+  }
+}
+
 app.get('*', function(req, res){
   var url = req.method === 'GET' ? req.query.url : req.body.url;
 
@@ -37,6 +55,10 @@ app.get('*', function(req, res){
     embed: req.query.embed ? true : false
   };
 
+  if( /\/embed/.test(req.url) ){
+    options.embed = true;
+  }
+
   if( url === undefined ){
     var error = 'URL is undefined';
     return res.json(422, { error: error });
@@ -44,23 +66,13 @@ app.get('*', function(req, res){
     url = decodeURIComponent(url);
   }
 
-  opengraph.get(options, function(err, graphProperties){
-    if( err ){
-      console.error('Error: '+ err +'|url: '+ url);
-      return res.json(500, { error: 'An error occurred processing: ' + url });
-    }
+  var context = {
+    req: req,
+    res: res,
+    url: url
+  };
 
-    if( req.query && req.query.callback ){
-      res.send(res.query.callback +'('+ JSON.stringify(graphProperties) +')');
-    } else {
-      if( graphProperties.embed ){
-        try {
-          graphProperties.embed = JSON.parse(graphProperties.embed);
-        } catch(e){ }
-      }
-      res.json(graphProperties);
-    }
-  });
+  opengraph.get(options, handleJSON.bind(context));
 });
 
 module.exports = app;
